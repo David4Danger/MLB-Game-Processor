639a11a4a0056434ce835d5830fb5ecaaa72f87d
David James Skudra <djskudra@uwaterloo.ca>
2021-03-17 19:16:15 -0400

    spring initlzr

---
+++ src/main/java/com/mlb/game/processor/MlbGameProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.mlb.game.processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MlbGameProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MlbGameProcessorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mlb/game/processor/MlbGameProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mlb.game.processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MlbGameProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
